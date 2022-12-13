a2338c94d7cfacd19137493f9a797467bd7334eb
shinafifloo <effati.afshin@gmail.com>
2022-12-13 18:07:15 -0500

    project1

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,16 @@
 from typing import List
 from pathlib import Path
 
-class Parser():
-    extensions = []
-    
+class Parser:
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+         return extension in self.extensions
+         
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
+        with open(path, 'r') as file:
+            
+
