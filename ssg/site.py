1c968dabffc05709a292e902ad6bc2d23987ee17
shinafifloo <effati.afshin@gmail.com>
2022-12-13 15:35:59 -0500

    project1

--- ssg/site.py
+++ ssg/site.py
@@ -6,6 +6,8 @@ class Site():
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest + "/" + path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
         
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
