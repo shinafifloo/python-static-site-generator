689b368f52280901076487a6ffcdcaa6920a5b49
shinafifloo <effati.afshin@gmail.com>
2022-12-13 19:02:06 -0500

    project1

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -21,4 +21,11 @@ class Parser:
             file.write(content)
 
     def copy(self, path, source, dest):
-        shutil.copy2(path, dest.relative_to(source))
+        shutil.copy2(path, dest / path.relative_to(source))
+
+
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(path, source, dest)
