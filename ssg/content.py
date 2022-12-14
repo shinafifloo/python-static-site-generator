11c421b99424fde6f94eba030f36cad758b35ce8
shinafifloo <effati.afshin@gmail.com>
2022-12-13 19:32:31 -0500

    project1

---
+++ ssg/content.py
@@ -0,0 +1,10 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    @classmethod
+    def Load(self)
