ff5ce654a572bd6b0422185a4d445a571baac732
shinafifloo <effati.afshin@gmail.com>
2022-12-13 19:19:03 -0500

    project1

--- ssg/site.py
+++ ssg/site.py
@@ -6,12 +6,20 @@ class Site():
         self.dest = Path(dest)
         self.parsers = parsers or []
 
+
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
         
+
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
