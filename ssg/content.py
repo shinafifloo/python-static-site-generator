ca401b184d53bebdb9e40ce492f36bc76dab62b4
shinafifloo <effati.afshin@gmail.com>
2022-12-13 20:19:08 -0500

    project1

--- ssg/content.py
+++ ssg/content.py
@@ -8,7 +8,7 @@ class Content(Mapping):
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
-    def Load(cls, string):
+    def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
